7dcbbe5975baf37a19222b36bbf491eef1e296a1
Kumar Nalin <145829962+nalintiwary18@users.noreply.github.com>
2026-02-14 21:48:53 +0530

    Update layout.tsx
    changing the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Gochi_Hand } from "next/font/google";
+import { Geist, Geist_Mono,Pangolin } from "next/font/google";
 
 import "./globals.css";
 import "katex/dist/katex.min.css";
